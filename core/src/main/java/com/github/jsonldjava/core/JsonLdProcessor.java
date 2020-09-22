4a76918a951e4d6a13c186b5d55de2052cf25e3a
Pascal Christoph <christoph@hbz-nrw.de>
2020-09-22 15:52:32 +0200

    Remove unused imports and comments
    See #296.

--- core/src/main/java/com/github/jsonldjava/core/JsonLdProcessor.java
+++ core/src/main/java/com/github/jsonldjava/core/JsonLdProcessor.java
@@ -2,11 +2,9 @@ package com.github.jsonldjava.core;
 
 import static com.github.jsonldjava.utils.Obj.newMap;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
